d692a1890eea41b2b24c43f175bd5b70b34d71a7
Pablo Martinez Palhares G. Villar <123903372+Pablogvz@users.noreply.github.com>
2023-03-07 17:50:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ function updateTitle() {
 }
 
 function initializeGame() {
-
-  vBoard = [['', '', ''], ['', '', ''], ['', '', '']];
-  turnPlayer = 'player1';
-  // Ajusta o título da página 
-  document.querySelector('h2').innerHTML = 'Vez de: <span id="turnPlayer"></span>';
+  // Inicializa as variáveis globais 
+  vBoard = [['', '', ''], ['', '', ''], ['', '', '']]
+  turnPlayer = 'player1'
+  // Ajusta o título da página (caso seja necessário)
+  document.querySelector('h2').innerHTML = 'Vez de: <span id="turnPlayer"></span>'
   updateTitle()
-  
+  // Limpa o tabuleiro (caso seja necessário) e adiciona os eventos de clique
   boardRegions.forEach(function (element) {
     element.classList.remove('win')
     element.innerText = ''
@@ -26,19 +26,19 @@ function initializeGame() {
 // Verifica se existem três regiões iguais em sequência e devolve as regiões
 function getWinRegions() {
   const winRegions = []
-  if (vBoard[0][0] && vBoard[0][0] === vBoard[0][1] && vBoard[0][0] === vBoard[0][2]);
+  if (vBoard[0][0] && vBoard[0][0] === vBoard[0][1] && vBoard[0][0] === vBoard[0][2])
     winRegions.push("0.0", "0.1", "0.2")
-  if (vBoard[1][0] && vBoard[1][0] === vBoard[1][1] && vBoard[1][0] === vBoard[1][2]);
+  if (vBoard[1][0] && vBoard[1][0] === vBoard[1][1] && vBoard[1][0] === vBoard[1][2])
     winRegions.push("1.0", "1.1", "1.2")
-  if (vBoard[2][0] && vBoard[2][0] === vBoard[2][1] && vBoard[2][0] === vBoard[2][2]);
+  if (vBoard[2][0] && vBoard[2][0] === vBoard[2][1] && vBoard[2][0] === vBoard[2][2])
     winRegions.push("2.0", "2.1", "2.2")
-  if (vBoard[0][0] && vBoard[0][0] === vBoard[1][0] && vBoard[0][0] === vBoard[2][0]);
+  if (vBoard[0][0] && vBoard[0][0] === vBoard[1][0] && vBoard[0][0] === vBoard[2][0])
     winRegions.push("0.0", "1.0", "2.0")
-  if (vBoard[0][1] && vBoard[0][1] === vBoard[1][1] && vBoard[0][1] === vBoard[2][1]);
+  if (vBoard[0][1] && vBoard[0][1] === vBoard[1][1] && vBoard[0][1] === vBoard[2][1])
     winRegions.push("0.1", "1.1", "2.1")
-  if (vBoard[0][2] && vBoard[0][2] === vBoard[1][2] && vBoard[0][2] === vBoard[2][2]);
+  if (vBoard[0][2] && vBoard[0][2] === vBoard[1][2] && vBoard[0][2] === vBoard[2][2])
     winRegions.push("0.2", "1.2", "2.2")
-  if (vBoard[0][0] && vBoard[0][0] === vBoard[1][1] && vBoard[0][0] === vBoard[2][2]);
+  if (vBoard[0][0] && vBoard[0][0] === vBoard[1][1] && vBoard[0][0] === vBoard[2][2])
     winRegions.push("0.0", "1.1", "2.2")
   if (vBoard[0][2] && vBoard[0][2] === vBoard[1][1] && vBoard[0][2] === vBoard[2][0])
     winRegions.push("0.2", "1.1", "2.0")
@@ -60,34 +60,34 @@ function handleWin(regions) {
 
 function handleBoardClick(ev) {
   // Obtém os índices da região clicada
-  const span = ev.currentTarget;
+  const span = ev.currentTarget
   const region = span.dataset.region // N.N
   const rowColumnPair = region.split('.') // ["N", "N"]
-  const row = rowColumnPair[0];
-  const column = rowColumnPair[1];
+  const row = rowColumnPair[0]
+  const column = rowColumnPair[1]
   // Marca a região clicada com o símbolo do jogador
   if (turnPlayer === 'player1') {
-    span.innerText = 'X';
-    vBoard[row][column] = 'X';
+    span.innerText = 'X'
+    vBoard[row][column] = 'X'
   } else {
-    span.innerText = 'O';
-    vBoard[row][column] = 'O';
+    span.innerText = 'O'
+    vBoard[row][column] = 'O'
   }
   // Limpa o console e exibe nosso tabuleiro virtual
-  console.clear();
-  console.table(vBoard);
+  console.clear()
+  console.table(vBoard)
   // Desabilita a região clicada
   disableRegion(span)
   // Verifica se alguém venceu
-  const winRegions = getWinRegions();
+  const winRegions = getWinRegions()
   if (winRegions.length > 0) {
     handleWin(winRegions)
   } else if (vBoard.flat().includes('')) {
-    turnPlayer = turnPlayer === 'player1' ? 'player2' : 'player1';
+    turnPlayer = turnPlayer === 'player1' ? 'player2' : 'player1'
     updateTitle()
   } else {
-    document.querySelector('h2').innerHTML = 'Empate!';
+    document.querySelector('h2').innerHTML = 'Empate!'
   }
 }
 // Adiciona o evento no botão que inicia o jogo
-document.getElementById('start').addEventListener('click', initializeGame);
+document.getElementById('start').addEventListener('click', initializeGame)
